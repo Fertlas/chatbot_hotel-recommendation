9d2e53bbdf72a540f2d3fc26bf564effc52dc79e
Fertlas <100676415+Fertlas@users.noreply.github.com>
2024-06-09 14:16:13 +0800

    Upload from my machine.

---
+++ chatbot.py
@@ -0,0 +1,36 @@
+import random
+from nlp_model import get_intent, load_nlp_model
+from recommendation_system import recommend_hotels
+
+bot_name = "Mumei"
+
+def get_response(intent, intents):
+    for i in intents['intents']:
+        if i['tag'] == intent:
+            return random.choice(i['responses'])
+
+def chat(intents, df):
+    clf, vectorizer = load_nlp_model()
+    print("Hello! How can I help you today?")
+    
+    while True:
+        user_input = input("You: ")
+        if user_input.lower() == 'quit':
+            break
+        
+        intent = get_intent(user_input, clf, vectorizer)
+        
+        if intent == 'recommend_hotel':
+            price_range = input("Mumei-Bot: Please specify the price range (e.g., 100-200): ")
+            district = input("Mumei-Bot: Please specify the district: ")
+            amenities = input("Mumei-Bot: Please specify the amenities (comma separated): ").split(',')
+            
+            recommended_hotels = recommend_hotels(df, price_range, district,  amenities)
+            if recommended_hotels.empty:
+                print("Mumei-Bot: Sorry, no hotels match your criteria.")
+            else:
+                print("Mumei-Bot: Here are some hotels that match your criteria:")
+                print(recommended_hotels[['Name', 'Room Rates', 'District', 'Room Types', 'Amenities']])
+        else:
+            response = get_response(intent, intents)
+            print(f"Mumei-Bot: {response}")
